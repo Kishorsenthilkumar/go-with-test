7e739eb44f50f9148ba5dec4c888fe1669b3ba9d
Kishorsenthilkumar <kishorsenthilkumar104@gmail.com>
2026-08-31 18:29:36 +0530

    implemented context-kicksoff longrunning process

---
+++ concurrency/context/context_test.go
@@ -0,0 +1,54 @@
+package context
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+func TestServer(t *testing.T) {
+	data := "Hello"
+	ser := Server(&SpyStore{data, false})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	ser.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got %s want %s", response.Body.String(), data)
+	}
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "Hello world"
+		store := &SpyStore{data, false}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingctx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingctx)
+
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
+		}
+
+	})
+}
