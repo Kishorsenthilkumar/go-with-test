127d5b4b8b96e01ced3d1fd5fb0c770b6ca34eb2
Kishorsenthilkumar <kishorsenthilkumar104@gmail.com>
2026-09-02 09:03:43 +0530

    file-handling with diff case

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -5,13 +5,21 @@ import (
 )
 
 type Post struct {
+	Title string
 }
 
-func NewPostsFromFs(FileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(FileSystem, ".")
-	var post []Post
-	for range dir {
-		post = append(post, Post{})
+func NewPostsFromFs(FileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(FileSystem, ".")
+
+	if err != nil {
+		return nil, err
 	}
-	return post
+	var posts []Post
+	for _, fname := range dir {
+		post, err := getPost(FileSystem, fname.Name())
+	}
+	if err != nil {
+		return nil, err
+	}
+
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -1,6 +1,7 @@
 package blogposts_test
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -12,9 +13,32 @@ func TestBlogPosts(t *testing.T) {
 		"hello.md":  {Data: []byte("hi")},
 		"hello1.md": {Data: []byte("how r u")},
 	}
-	posts := blogposts.NewPostsFromFs(fs)
+	posts, err := blogposts.NewPostsFromFs(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(fs) != len(posts) {
 		t.Errorf("got %d wanted %d", len(posts), len(fs))
 	}
+}
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello.md":  {Data: []byte("Title: Post1")},
+		"hello1.md": {Data: []byte("Title: Post2")},
+	}
+	posts, err := blogposts.NewPostsFromFs(fs)
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
