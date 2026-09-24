8b28f63b633696ee65c95b70c741411a94ad7ae6
Kishorsenthilkumar <kishorsenthilkumar104@gmail.com>
2026-09-24 17:22:40 +0530

    handler refactor

--- web/snippetbox/code/main.go
+++ web/snippetbox/code/main.go
@@ -1,161 +1,32 @@
 package main
 
 import (
-	"bytes"
+	"crypto/tls"
 	"database/sql"
-	"errors"
 	"flag"
-	"fmt"
-	"go-with-test/web/snippetbox/internal/Validator"
-	"go-with-test/web/snippetbox/internal/models"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
+
+	"go-with-test/web/snippetbox/internal/models"
 
 	"github.com/alexedwards/scs/mysqlstore"
 	"github.com/alexedwards/scs/v2"
-	"github.com/julienschmidt/httprouter"
+	_ "github.com/go-sql-driver/mysql"
 )
-
-type application struct {
-	errorlog       *log.Logger
-	infolog        *log.Logger
-	snippets       *models.SnippetModel
-	templateCache  map[string]*template.Template
-	sessionManager *scs.SessionManager
-}
-
-type snippetCreateForm struct {
-	Title   string
-	Content string
-	Expires int
-	Validator.Validator
-}
-
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
-	snippets, err := app.snippets.Latest()
-
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	data := app.newTemplateData(r)
-	data.Snippets = snippets
-
-	app.render(w, http.StatusOK, "home.tmpl", data)
-
-}
-
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.Atoi(params.ByName("id"))
-
-	if err != nil || id < 1 {
-		app.notFound(w)
-		return
-	}
-
-	snippet, err := app.snippets.Get(id)
-
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
-		} else {
-			app.serverError(w, err)
-		}
-		return
-	}
-	data := app.newTemplateData(r)
-	data.Snippet = snippet
-	app.render(w, http.StatusOK, "view.tmpl", data)
-
-}
-
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	data := app.newTemplateData(r)
-	data.Form = snippetCreateForm{
-		Expires: 365,
-	}
-}
-
-func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
-
-	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	form := snippetCreateForm{
-		Title:   r.PostForm.Get("title"),
-		Content: r.PostForm.Get("content"),
-		Expires: expires,
-	}
-
-	form.CheckField(Validator.NotBlank(form.Title), "title", "This field cannot be blank")
-	form.CheckField(Validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
-	form.CheckField(Validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(Validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
-
-	if !form.Valid() {
-		data := app.newTemplateData(r)
-		data.Form = form
-		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
-		return
-	}
-
-	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
-
-	if err != nil {
-		app.serverError(w, err)
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
-
-}
 
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
 	}
+
 	if err = db.Ping(); err != nil {
 		return nil, err
 	}
+
 	return db, nil
-}
-
-func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
-
-	ts, ok := app.templateCache[page]
-	if !ok {
-		err := fmt.Errorf("the template %s does not exist", page)
-		app.serverError(w, err)
-		return
-	}
-
-	buff := new(bytes.Buffer)
-
-	err := ts.ExecuteTemplate(buff, "base", data)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	w.WriteHeader(status)
-	buff.WriteTo(w)
 }
 
 func main() {
@@ -185,6 +56,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorlog:       errorlog,
@@ -194,14 +66,22 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorlog,
-		Handler:  app.routes(),
+		Addr:         *addr,
+		ErrorLog:     errorlog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infolog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 
 	errorlog.Fatal(err)
 
