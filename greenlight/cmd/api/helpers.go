a84b54271fbdf1a2fad281a66802cea1ff687038
Kishorsenthilkumar <kishorsenthilkumar104@gmail.com>
2026-09-26 16:37:53 +0530

    json encoding/struct tags

---
+++ greenlight/cmd/api/helpers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, header http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+
+	for key, value := range header {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"greenlight.alexedwards.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +22,17 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
